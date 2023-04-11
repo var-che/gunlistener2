910fa1ca3d662fad397dbc943c7a7d5d2935aae1
var-che <filo.vlada@gmail.com>
2023-04-11 21:02:42 +0200

    added route and redata

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const Gun = require('gun')
 const app = express()
 const port = 8000
 app.use(Gun.serve)
-
+app.get('/test', (req, res) => {
+  res.send('hello world')
+})
 const server = app.listen(port, () => {
     console.log("Listening at: http://localhost:" + port)
 })
